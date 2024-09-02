cafb788798ef1e6291f61af9af4f70342d90ae12
Arinhakeo <9295@holbertonstudents.com>
2024-09-02 12:35:38 +0200

    allfiles

---
+++ 3-print_number.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+# Déclaration de la variable contenant le nombre flottant
+number = 3.14159
+
+# Utilisation des f-strings pour formater et imprimer le nombre avec une précision de 2 décimales
+print(f"Float: {number:.2f}")
