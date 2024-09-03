8d9d501f634944cbcb8d0f15e2daba3f523880c9
Arinhakeo <9295@holbertonstudents.com>
2024-09-04 01:12:36 +0200

    factorial.py

--- debugging/factorial.py
+++ debugging/factorial.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 import sys
 
+
 def factorial(n):
     result = 1
     while n > 1:
@@ -8,8 +9,7 @@ def factorial(n):
         n -= 1
     return result
 
+
 if len(sys.argv) > 1:
     f = factorial(int(sys.argv[1]))
     print(f)
-else:
-    print("Usage: ./factorial.py <number>")
